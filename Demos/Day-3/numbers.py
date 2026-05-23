72b5238e4b83f4521210562f1e25c8df5d6a5bb5
anindita.basak@live.com <anindita.basak@live.com>
2026-05-23 12:20:05 +0530

    added py numbers

---
+++ Demos/Day-3/numbers.py
@@ -0,0 +1,10 @@
+# modulo operator (%) -> represented by % symbol calculates the remainder when one number is divided by another
+
+result = 9 % 2
+print(result)
+
+# Power Operator raises a number to a specified exponent
+
+result = 2 ** 3
+print(result)
+
