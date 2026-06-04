3d45f1a4c3bcb3b7459a6a295a29301e863531cd
anindita.basak@live.com <anindita.basak@live.com>
2026-06-04 12:23:47 +0530

    added netmiko lab

---
+++ Demos/Day-10/my-netmiko.py
@@ -0,0 +1,14 @@
+from netmiko import ConnectHandler
+
+cisco_router_telnet = {
+    "device_type": "cisco_ios_telnet",
+    "host": "192.168.80.129",
+    "port": "5000",
+    "username": "admin",
+    "password": "cisco123",
+}
+
+net_connect = ConnectHandler(**cisco_router_telnet)
+output = net_connect.send_Command("show ip interface brief")
+print(output)
+net_connect.disconnect()
