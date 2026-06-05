abbf7e7bc4297184ff0325424e51aec8bf573c13
anindita.basak@live.com <anindita.basak@live.com>
2026-06-05 11:11:37 +0530

    added netmiko sequentially commands

--- Demos/Day-10/my-netmiko.py
+++ Demos/Day-10/my-netmiko.py
@@ -11,4 +11,9 @@ cisco_router_telnet = {
 net_connect = ConnectHandler(**cisco_router_telnet)
 output = net_connect.send_Command("show ip interface brief")
 print(output)
+# running multiple commands sequentially 
+commands = ['show version', 'show ip route', 'show interfaces']
+for command in commands:
+    output = net_connect.send_command(command)
+    print(output)
 net_connect.disconnect()
